a1c242bc1b727ae644325b2dde3e065c9b0ec56b
João Pedro <joaopsilvamagalhaes@gmail.com>
2022-01-22 19:36:48 -0300

    chore: add react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import type { AppProps as NextAppProps } from 'next/app';
 import type { NextPage } from 'next';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 import { Toaster } from 'react-hot-toast';
 
@@ -16,17 +19,32 @@ type AppProps = NextAppProps & {
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+          },
+        },
+      })
+  );
   const withHeader = Component.withHeader ?? true;
 
   return (
-    <Providers>
-      <ThemeProvider theme={theme}>
-        {withHeader && <Header />}
-        <Component {...pageProps} />
-        <Toaster />
-        <GlobalStyles />
-      </ThemeProvider>
-    </Providers>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Providers>
+          <ThemeProvider theme={theme}>
+            {withHeader && <Header />}
+            <Component {...pageProps} />
+            <Toaster />
+            <GlobalStyles />
+          </ThemeProvider>
+        </Providers>
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
